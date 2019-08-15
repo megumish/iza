ce6d5468c2ea308fcd06175a9905181c3b175030
Keishi Kawada <megumish@exploitouri.st>
2019-08-16 07:44:48 +0900

    implement new-package method

--- src/package/package_app.rs
+++ src/package/package_app.rs
@@ -1,6 +1,10 @@
 use crate::package::*;
 
-pub trait PackageApp: HasPackageRepository {}
+pub trait PackageApp: HasPackageRepository {
+    fn new_package(&self, name: String, working_directory: String) -> Result<()> {
+        Ok(Package::new(name, working_directory)).and_then(|p| self.package_repository().push(&p))
+    }
+}
 
 pub trait HasPackageApp {
     type App: PackageApp;
