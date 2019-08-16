bd21c4a2e3189eeaf1fa1a029eea96dd973f8179
Keishi Kawada <megumish@exploitouri.st>
2019-08-17 05:27:55 +0900

    remove SSHAuthentication

--- src/ssh_authentication/error.rs
+++
@@ -1,3 +0,0 @@
-pub enum Error {}
-
-pub type Result<T> = std::result::Result<T, Error>;
--- src/ssh_authentication/remote_path.rs
+++
@@ -1,9 +0,0 @@
-pub struct RemotePath {
-    path_string: String,
-}
-
-impl From<String> for RemotePath {
-    fn from(path_string: String) -> Self {
-        Self { path_string }
-    }
-}
