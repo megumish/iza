06f0586d9ce5d43f2164ecd2b04bb163ece0a8b8
Keishi Kawada <megumish@exploitouri.st>
2019-08-17 20:36:31 +0900

    Default Implement System Directory

--- src/system_directory/error.rs
+++ src/system_directory/error.rs
@@ -1,3 +1,13 @@
-pub enum Error {}
+pub enum Error {
+    InvalidTopPath,
+    IOError,
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        eprintln!("{}", error);
+        Error::IOError
+    }
+}
